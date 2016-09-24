1897ec4a8c2677222d0529eae8939bb32a4c581c
Peter Vingelmann <petya@master.com>
2016-09-24 03:11:56 +0200

    No indent for namespaces

--- src/aybabtu/base64.hpp
+++ src/aybabtu/base64.hpp
@@ -11,23 +11,23 @@
 
 namespace aybabtu
 {
-    struct base64
-    {
+struct base64
+{
 
-        /// Creates a base64 encoded string from a vector.
-        ///
-        /// @param buffer A vector containing the data to encode.
-        static std::string encode(const std::vector<uint8_t>& buffer);
+    /// Creates a base64 encoded string from a vector.
+    ///
+    /// @param buffer A vector containing the data to encode.
+    static std::string encode(const std::vector<uint8_t>& buffer);
 
-        /// Creates a base64 encoded string from a buffer of the specified size.
-        ///
-        /// @param data a pointer to the data
-        /// @param size the size of the data in bytes
-        static std::string encode(const uint8_t* data, uint32_t size);
+    /// Creates a base64 encoded string from a buffer of the specified size.
+    ///
+    /// @param data a pointer to the data
+    /// @param size the size of the data in bytes
+    static std::string encode(const uint8_t* data, uint32_t size);
 
-        /// Decodes a base64 encoded string to a vector.
-        ///
-        /// @param encoded_data A string containing the encoded data.
-        static std::vector<uint8_t> decode(const std::string& encoded_data);
-    };
+    /// Decodes a base64 encoded string to a vector.
+    ///
+    /// @param encoded_data A string containing the encoded data.
+    static std::vector<uint8_t> decode(const std::string& encoded_data);
+};
 }
